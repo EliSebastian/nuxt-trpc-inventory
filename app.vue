956fb7e083494fbd28a5c33ef3e7da89606ee6aa
Eli Herrera <esrbastianherrera@gmail.com>
2024-02-01 18:17:53 -0600

    add trpc

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+const { $client } = useNuxtApp()
+const text = ref('world')
+
+const back = ref();
+
+// add a watch to the text ref
+watch(text, async (newVal, oldVal) => {
+  back.value = await $client.hello.useQuery({ text: newVal })
+
+})
+
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Hello World</h1>
+    <input type="text" v-model="text">
+    <h2>{{ text }}</h2>
+    <h2>{{ back }}</h2>
   </div>
 </template>
