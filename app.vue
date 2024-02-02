64ecbf0bf870d5dd7c98b6899d2e06bf4fa19f5a
Eli Herrera <esrbastianherrera@gmail.com>
2024-02-02 10:39:38 -0600

    add tailwind and layout

--- app.vue
+++ app.vue
@@ -1,22 +1,10 @@
 <script setup lang="ts">
-const { $client } = useNuxtApp()
-const text = ref('world')
 
-const back = ref();
-
-// add a watch to the text ref
-watch(text, async (newVal, oldVal) => {
-  back.value = await $client.hello.useQuery({ text: newVal })
-
-})
-
+console.log('Hello, World!');
 </script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <input type="text" v-model="text">
-    <h2>{{ text }}</h2>
-    <h2>{{ back }}</h2>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,16 @@ export default defineNuxtConfig({
   build: {
     transpile: ["trpc-nuxt"],
   },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 });
